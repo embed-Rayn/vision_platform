afd46e7c14b3fbeb68551febb15a6c49f07bc75d
BJ Park <qwe1443@gmail.com>
2023-02-26 19:08:06 +0900

    update view.py(index view: shortcut:render)

--- back_django/polls/urls.py
+++ back_django/polls/urls.py
@@ -4,4 +4,25 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+# path()의 파라미터
+"""route
+- route 는 URL 패턴을 가진 문자열.  
+- 요청이 처리될 때, urlpatterns 의 첫 번째 패턴부터 시작하여, 일치하는 패턴을 찾을 때 까지 요청된 URL 을 각 패턴과 리스트의 순서대로 비교.
+- 패턴들은 GET 이나 POST 의 매개 변수들, 혹은 도메인 이름을 검색X. 
+    - 예를 들어, https://www.example.com/myapp/ 이 요청된 경우, URLconf 는 오직 myapp/ 부분만 바라 봅니다. 
+    - https://www.example.com/myapp/?page=3, 같은 요청에도, URLconf 는 역시 myapp/ 부분만 신경씁니다.
+"""
+"""view
+- Django 에서 일치하는 패턴을 찾으면, HttpRequest 객체를 첫번째 인수로 하고, 경로로 부터 ‘캡처된’ 값을 키워드 인수로하여 특정한 view 함수를 호출. 
+"""
+"""kwargs
+- 임의의 키워드 인수들은 목표한 view 에 사전형으로 전달
+"""
+"""name
+- URL 에 이름을 지으면, 템플릿을 포함한 Django 어디에서나 명확하게 참조할 수 있습니다. 
+- 이 강력한 기능을 이용하여, 단 하나의 파일만 수정해도 project 내의 모든 URL 패턴을 바꿀 수 있도록 도와줍니다.
+"""
