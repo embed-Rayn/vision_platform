dd869f31d69bc240719b7896b053dd6bb45e9b93
BJ Park <qwe1443@gmail.com>
2023-02-28 00:21:51 +0900

    add 404 error

--- back_django/polls/views.py
+++ back_django/polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 
 
 from .models import Question
@@ -9,11 +8,17 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
+    # loader + HttopResponse == shortcut.render
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}."
